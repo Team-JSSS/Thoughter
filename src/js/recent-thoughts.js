99bb3a50b3822bc796e955524afe492d275b967e
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-13 14:36:09 -0400

    changed double quotes to single in recent thoughs js

--- src/js/recent-thoughts.js
+++ src/js/recent-thoughts.js
@@ -6,14 +6,14 @@
   window.thoughtApp.buildList = function buildList(data) {
     data.forEach(function createThoughts(thought) {
       $( 'main' )
-        .append( `<section class="panel panel-info">
+        .append( `<section class='panel panel-info'>
 
-          <aside class="panel-heading">
+          <aside class='panel-heading'>
             <!-- set margin on asides to 0 -->
-            <h3 class="panel-title">Posted at [insert time here] on [date]</h3>
+            <h3 class='panel-title'>Posted at [insert time here] on [date]</h3>
 
           </aside>
-          <aside class="panel-body">
+          <aside class='panel-body'>
             <p>${thought.content}</p>
           </aside>
         </section>` );
