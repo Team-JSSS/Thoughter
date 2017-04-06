83466e8192794d5fb17238961d483220a4ceaed6
Steven Strasburg <SSStrasburg@gmail.com>
2017-04-06 08:14:42 -0400

    added the build file from grunt

---
+++ gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function configureGrunt(gruntConfig) {
+
+
+  gruntConfig.initConfig(   {
+
+    clean: ['build/'],
+
+    copy: {
+      copyhtml: {
+        files: [
+          {
+          cwd: 'src/',
+          src: ['*.html'],
+          dest: 'build/',
+          expand: true
+          }
+        ]
+      },
+
+      copyjs: {
+        files: [
+          {
+            cwd:'src/',
+            src: ['**/*.js'],
+            dest: 'build/js/',
+            expand: true
+          }
+        ]
+      },
+
+      copyjq: {
+        files: [
+          {
+            cwd: 'node_modules/jquery/dist/',
+            src: ['jquery.js'],
+            dest: 'build/js/vendor/',
+            expand: true
+          }
+        ]
+      }
+    },
+
+    sass: {
+      all: {
+        files: {
+          'build/style.css' : 'src/css/sass/main.scss'
+        }
+      }
+
+    },
+
+    jshint: {
+      appjs: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        files: {
+          src: ['/.jshintrc.js']
+        }
+      }
+    },
+
+    karma: {
+      all: {
+        options: {
+          frameworks: ['mocha', 'chai'],
+          browsers: ['Chrome'],
+          singleRun: true,
+          files: [
+            'src/**/*.js',
+            'test/specs/**/*.js'
+          ]
+        }
+      }
+    }
+
+  });
+  require('load-grunt-tasks')(gruntConfig);
+  gruntConfig.loadNpmTasks('grunt-contrib-watch');
+  gruntConfig.registerTask('build', ['jshint', 'clean', 'copy', 'sass', 'karma']);
+
+
+};
